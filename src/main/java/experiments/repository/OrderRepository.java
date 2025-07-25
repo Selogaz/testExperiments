82ddcebcd851f810cfa871dd1eb63e23fe579bcd
Selogaz <Terentjev.selogaz@yandex.ru>
2025-07-25 06:48:00 +0300

    ConcurrentHashMap and catch in save() added

--- src/main/java/experiments/repository/OrderRepository.java
+++ src/main/java/experiments/repository/OrderRepository.java
@@ -5,27 +5,28 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.locks.ReentrantLock;
 
 @Repository
 public class OrderRepository {
-    private final List<OrderModel> orders = new ArrayList<>();
+    private final ConcurrentHashMap<Long, OrderModel> orders = new ConcurrentHashMap();
     private final AtomicLong idCounter = new AtomicLong(1);
-    private final ReentrantLock lock = new ReentrantLock();
 
     public OrderModel save(OrderModel order) {
-        lock.lock();
         try {
+            Long id = order.getId();
             OrderModel newOrder = new OrderModel(idCounter.getAndIncrement(), order.getCustomerName(), order.getTotalAmount());
-            orders.add(newOrder);
+            orders.put(id, newOrder);
             return newOrder;
-        } finally {
-            lock.unlock();
+        } catch (Exception e) {
+            System.out.println("Ошибка при сохранении заказа с id " + order.getId());
+            e.printStackTrace();
+            return null;
         }
     }
 
     public List<OrderModel> findAll() {
-        return new ArrayList<>(orders);
+        return new ArrayList<>(orders.values());
     }
 }
